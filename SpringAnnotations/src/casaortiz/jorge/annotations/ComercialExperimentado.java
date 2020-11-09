9f6fc12d538b737075f48559c265ce8095b178d3
jorgeortizc05 <jorgeortizc05@gmail.com>
2020-11-09 17:19:13 -0500

    Java Annotations: Autowired

--- SpringAnnotations/src/casaortiz/jorge/annotations/ComercialExperimentado.java
+++ SpringAnnotations/src/casaortiz/jorge/annotations/ComercialExperimentado.java
@@ -6,21 +6,34 @@ import org.springframework.stereotype.Component;
 //Creo la annotations con @component
 @Component //("miNombre")si no das el nombre del bean, por default utiliza nombre de la clase con la primera letra en minuscula
 public class ComercialExperimentado implements Empleados {
-
+	
+	@Autowired //Inyeccion de dependencias: aqui es mas facil, sin constructor ni setter
 	private GeneracionInformeFinanciero nuevoInforme;
 	
-	//Inyectamos con @Autowired: este es el metodo constructor
+	/*//Inyectamos con @Autowired: este es el metodo constructor
 	//autowired: Spring busca en todo el proyecto a ver si hay una clase que implemente GeneracionInformeFinanciero, en este
-	//caso InformeFinancieroTrim lo implementa y lo inyecta
-	@Autowired 
+	//caso InformeFinancieroTrim tiene la interfaz, lo implementa y lo inyecta
+	@Autowired  //Spring 4.3: si es un solo contructor, no es necesario implementar el @Autowired
 	public ComercialExperimentado(GeneracionInformeFinanciero nuevoInforme) {
 		this.nuevoInforme = nuevoInforme;
+	}*/
+	
+	
+	
+	public ComercialExperimentado() {
+		
 	}
 	
+	//inyectamos, metodo setter
+	/*@Autowired
+	public void setNuevoInforme(GeneracionInformeFinanciero nuevoInforme) {
+		this.nuevoInforme = nuevoInforme;
+	}*/
+
 	@Override
 	public String getTareas() {
 		// TODO Auto-generated method stub
-		return "Vender, vender y vender m·s";
+		return "Vender, vender y vender m√°s";
 	}
 
 	@Override
--- SpringAnnotations/src/casaortiz/jorge/annotations/InformeFinancieroTrim1.java
+++ SpringAnnotations/src/casaortiz/jorge/annotations/InformeFinancieroTrim1.java
@@ -8,7 +8,7 @@ public class InformeFinancieroTrim1 implements GeneracionInformeFinanciero {
 	@Override
 	public String getInformeFinanciero() {
 		// TODO Auto-generated method stub
-		return "Presentacion de informe financiero del trimestre1";
+		return "Presentacion de informe financiero del trimestre 1";
 	}
 
 }
